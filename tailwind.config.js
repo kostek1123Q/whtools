06471e8799a952716f4263c85721f4d2ed369395
kostek1123Q <pokef227@gmail.com>
2025-06-30 23:32:48 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class', // włączamy tryb ciemny przez dodanie klasy 'dark' na <html> lub <body>
+  darkMode: 'class',
   content: [
-    './index.html',
-    './src/**/*.{js,jsx,ts,tsx}', // skanuj pliki w src dla klas tailwind
+    "./index.html",
+    "./src/**/*.{js,jsx}"
   ],
   theme: {
-    extend: {
-      // jeśli chcesz dodać własne kolory w stylu WhatsApp:
-      colors: {
-        'whatsapp-green-light': '#25D366',
-        'whatsapp-green-dark': '#075E54',
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
